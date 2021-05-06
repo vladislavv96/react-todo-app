1a53330788b8356a84cfa53910c486fb51c74836
Vladislav <vladislavv@protonmail.com>
2021-05-07 01:59:07 +0500

    add basic layot and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
   return ( 
     <div className = "App" >
-      <h1>Hello React</h1>
+      <header>
+        <h1>Vladislav's Todo List</h1>
+      </header>
+      <Form/>
+      <TodoList/>
     </div>
   );
 }
