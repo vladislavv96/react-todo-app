5b77b0fccf10f62c1b25533fe1fbd870d47d3153
Vladislav <vladislavv@protonmail.com>
2021-05-10 21:15:49 +0500

    add input and Todo states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React, {useState} from 'react'
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
   return ( 
     <div className = "App" >
       <header>
         <h1>Vladislav's Todo List</h1>
       </header>
-      <Form/>
-      <TodoList/>
+      <Form inputText={inputText} todos={todos} setInputText={setInputText} setTodos={setTodos}/>
+      <TodoList />
     </div>
   );
 }
